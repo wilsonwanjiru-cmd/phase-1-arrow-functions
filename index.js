bda8c65c1f69fe8c08d23e95a239b57b2cbac1cc
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-06 14:38:10 +0300

    <Arrow Functions>

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+// Function expression called divide
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Arrow function called square
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  };
+  
